f2380d16a34eb50715cc682e8b4c1149c80307f0
Charlie Comeau <comchara@amazon.com>
2021-12-17 15:59:15 -0800

    adding projects

---
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Comeau, Charlie on 10/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
